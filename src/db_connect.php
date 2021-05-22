6fa88aa9a8fae8cb3c70e1621edc5fd62d0b90d4
nkhck <78629044+nkhck@users.noreply.github.com>
2021-05-23 07:37:39 +0900

    Update db_connect.php

--- src/db_connect.php
+++ src/db_connect.php
@@ -2,9 +2,9 @@
 //データベースへの接続
 try 
 {
-  $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-  $user = 'root';
-  $pass = 'root';
+  $dsn = 'mysql:dbname="DB名";host=localhost;charset=utf8';
+  $user = 'ユーザー名';
+  $pass = 'パスワード';
   $dbh = new PDO($dsn, $user, $pass);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
